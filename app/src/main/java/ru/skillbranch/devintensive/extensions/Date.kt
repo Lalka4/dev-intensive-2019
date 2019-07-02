5139deb67867aa75addf60887a4936ae1003c67a
Lalka4 <gt5570a54321z@gmail.com>
2019-07-02 13:03:35 +0300

    hometask_2 version 3

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -35,71 +35,71 @@ fun Date.add(value:Int, units: TimeUnits = TimeUnits.SECOND): Date{
 }
 
 
-fun Date.humanizeDiff(date:Date = Date()) {
+fun Date.humanizeDiff(date:Date = Date()):String {
     val diffSeconds = ((date.time - this.time) / 1000).toInt()
     val days = diffSeconds / 86400
     val hours = diffSeconds / 3600
     val minutes = diffSeconds / 60
 
     if (diffSeconds >= 0) {
-        if (diffSeconds >= 1) {
-            if (diffSeconds >= 45) {
-                if (diffSeconds >= 75) {
-                    if (minutes >= 45) {
-                        if (minutes >= 75) {
-                            if (hours >= 22) {
-                                if (hours >= 26) {
-                                    if (days >= 360) {
-                                        println("более года назад")
+        if (diffSeconds > 1) {
+            if (diffSeconds > 45) {
+                if (diffSeconds > 75) {
+                    if (minutes > 45) {
+                        if (minutes > 75) {
+                            if (hours > 22) {
+                                if (hours > 26) {
+                                    if (days > 360) {
+                                        return "более года назад"
                                     } else when (days) {
-                                        in 1..360 step 10 -> println("$days день назад")
-                                        in 2..360 step 10, in 3..360 step 10, in 4..360 step 10 -> println("$days дня назад")
-                                        else -> println("$days дней назад")
+                                        in 1..360 step 10 -> return "${abs(days)} день назад"
+                                        in 2..360 step 10, in 3..360 step 10, in 4..360 step 10 -> return "${abs(days)} дня назад"
+                                        else -> return "${abs(days)} дней назад"
                                     }
-                                } else println("день назад")
+                                } else return "день назад"
                             } else when (hours) {
-                                1, 21 -> println("$hours час назад")
-                                2, 3, 4, 22 -> println("$hours часа назад")
-                                else -> println("$hours часов назад")
+                                1, 21 -> return "${abs(hours)} час назад"
+                                2, 3, 4, 22 -> return "${abs(hours)} часа назад"
+                                else -> return "${abs(hours)} часов назад"
                             }
-                        } else println("час назад")
+                        } else return "час назад"
                     } else when (minutes) {
-                        in 1..60 step 10 -> println("$minutes минуту назад")
-                        in 2..60 step 10, in 3..60 step 10, in 4..60 step 10 -> println("$minutes минуты назад")
-                        else -> println("$minutes минут назад")
+                        in 1..60 step 10 -> return "${abs(minutes)} минуту назад"
+                        in 2..60 step 10, in 3..60 step 10, in 4..60 step 10 -> return "${abs(minutes)} минуты назад"
+                        else -> return "${abs(minutes)} минут назад"
                     }
-                } else println("минуту назад")
-            } else println("несколько секунд назад")
-        } else println("только что")
+                } else return "минуту назад"
+            } else return "несколько секунд назад"
+        } else return "только что"
     } else {
-        if (diffSeconds <= -1) {
-            if (diffSeconds <= -45) {
-                if (diffSeconds <= -75) {
-                    if (minutes <= -45) {
-                        if (minutes <= -75) {
-                            if (hours <= -22) {
-                                if (hours <= -26) {
-                                    if (days <= -360) {
-                                        println("более чем через год")
+        if (diffSeconds < -1) {
+            if (diffSeconds < -45) {
+                if (diffSeconds < -75) {
+                    if (minutes < -45) {
+                        if (minutes < -75) {
+                            if (hours < -22) {
+                                if (hours < -26) {
+                                    if (days < -360) {
+                                        return "более чем через год"
                                     } else when (days) {
-                                        in -351..-1 step 10 -> println("через ${abs(days)} день")
-                                        in -352..-2 step 10, in -353..-3 step 10, in -354..-4 step 10 -> println("через ${abs(days)} дня")
-                                        else -> println("через ${abs(days)} дней")
+                                        in -351..-1 step 10 -> return "через ${abs(days)} день"
+                                        in -352..-2 step 10, in -353..-3 step 10, in -354..-4 step 10 -> return "через ${abs(days)} дня"
+                                        else -> return "через ${abs(days)} дней"
                                     }
-                                } else println("через день")
+                                } else return "через день"
                             } else when (hours) {
-                                -1, -21 -> println("через ${abs(hours)} час")
-                                -2, -3, -4, -22 -> println("через ${abs(hours)} часа")
-                                else -> println("через ${abs(hours)} часов")
+                                -1, -21 -> return "через ${abs(hours)} час"
+                                -2, -3, -4, -22 -> return "через ${abs(hours)} часа"
+                                else -> return "через ${abs(hours)} часов"
                             }
-                        } else println("час назад")
+                        } else return "час назад"
                     } else when (minutes) {
-                        in -51..-1 step 10 -> println("через ${abs(minutes)} минуту")
-                        in -52..-2 step 10, in -53..-3 step 10, in -54..-4 step 10 -> println("через ${abs(minutes)} минуты")
-                        else -> println("через ${abs(minutes)} минут")
+                        in -51..-1 step 10 -> return "через ${abs(minutes)} минуту"
+                        in -52..-2 step 10, in -53..-3 step 10, in -54..-4 step 10 -> return "через ${abs(minutes)} минуты"
+                        else -> return "через ${abs(minutes)} минут"
                     }
-                } else println("через минуту")
-            } else println("через несколько секунд")
-        } else println("только что")
+                } else return "через минуту"
+            } else return "через несколько секунд"
+        } else return "только что"
     }
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -8,28 +8,53 @@ object Utils {
         return firstName to lastName
     }
 
-    fun toInitials(firstName:String?, lastName:String?):String? = "${firstName?.get(0)?: ""}".toUpperCase()+
-            "${lastName?.get(0) ?: ""}".toUpperCase()
+    fun toInitials(firstName:String?, lastName:String?):String? {
 
-    var liters: Map<String, String> = mapOf("а" to "a", "б" to "b", "в" to "v", "г" to "g", "д" to "d", "е" to "e",
-        "ё" to "e", "ж" to "zh", "з" to "z", "и" to "i", "й" to "i", "к" to "k", "л" to "l", "м" to "m", "н" to "n",
+        when(firstName?.trim()){
+            null, "", " "->when(lastName?.trim()){
+                null, "", " "->return null
+                else -> return "${lastName.get(0)}".toUpperCase()
+            }
+            else->when(lastName?.trim()){
+                null, "", " " ->return "${firstName.get(0)}".toUpperCase()
+                else -> return "${firstName.get(0)}".toUpperCase() + "${lastName.get(0)}".toUpperCase()
+            }
+        }
+    }
+
+    var liters: Map<String, String> = mapOf("а" to "a", "А" to "A", "б" to "b", "Б" to "B", "в" to "v", "В" to "V", "г" to "g", "Г" to "G", "д" to "d", "Д" to "D", "е" to "e", "Е" to "E",
+        "ё" to "e", "Ё" to "e", "ж" to "zh", "з" to "z", "и" to "i", "й" to "i", "к" to "k", "л" to "l", "м" to "m", "н" to "n",
         "о" to "o", "п" to "p", "р" to "r", "с" to "s", "т" to "t", "у" to "u", "ф" to "f", "х" to "h", "ц" to "c",
         "ч" to "ch", "ш" to "sh", "щ" to "sh\'", "ъ" to "", "ы" to "i", "ь" to "", "э" to "e", "ю" to "yu", "я" to "ya")
 
-    fun transliteration(payload:String, divider:String? = " "):String?{
-        val str:List<String> = payload.split(" ")
-        val firstName:List<String> = str[0].toLowerCase().split("")
-        val lastName:List<String> = str[1].toLowerCase().split("")
-        var newFirstName= ""
-        var newLastName= ""
-        
-        for (char in firstName){
-            newFirstName+=liters[char]?:char
+    fun transliteration(payload:String, divider:String = " "):String?{
+        if (divider == "" || !payload.contains(" ")){
+            val firstName = payload.toCharArray()
+            var newFirstName = ""
+            for (char in firstName) {
+                if (char.isUpperCase()) {
+                    newFirstName += liters[char.toString().toLowerCase()]?.capitalize() ?: char.toString()
+                } else newFirstName += liters[char.toString().toLowerCase()] ?: char.toString()
+            }
+            return "$newFirstName"
         }
-        for (char in lastName){
-            newLastName += liters[char]?:char
+        else {
+            val str: List<String> = payload.split(" ")
+            val firstName = str[0].toCharArray()
+            val lastName = str[1].toCharArray()
+            var newFirstName = ""
+            var newLastName = ""
+            for (char in firstName) {
+                if (char.isUpperCase()) {
+                    newFirstName += liters[char.toString().toLowerCase()]?.capitalize() ?: char.toString()
+                } else newFirstName += liters[char.toString().toLowerCase()] ?: char.toString()
+            }
+            for (char in lastName) {
+                if (char.isUpperCase()) {
+                    newLastName += liters[char.toString().toLowerCase()]?.capitalize() ?: char.toString()
+                } else newLastName += liters[char.toString().toLowerCase()] ?: char.toString()
+            }
+            return "$newFirstName$divider$newLastName"
         }
-
-        return "${newFirstName.dropLast(newFirstName.count()-1).toUpperCase()}${newFirstName.drop(1)}$divider${newLastName.dropLast(newLastName.count()-1).toUpperCase()}${newLastName.drop(1)}"
     }
 }
